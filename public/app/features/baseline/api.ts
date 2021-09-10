8b22e4b453aeaa5b02836e8aedbcd21a8bfbc30f
andre-vidal <avidal@realdecoy.com>
2021-09-10 10:06:24 +0000

    hotfix: update api method calls - remove simulation data

--- public/app/features/baseline/api.ts
+++ public/app/features/baseline/api.ts
@@ -1,35 +1,15 @@
-// import { getBackendSrv } from '@grafana/runtime';
-
 import { BaselineEntryFields } from './types';
 import { BaselineDTO, BaselineDatasource } from '../../types';
-let baselineRecords: BaselineDTO[] = [];
 
 const baselineQuery = new BaselineDatasource();
 
-function loadBaselineEntries(): Promise<BaselineDTO[]> {
-  // TODO: replace fabircated promise with commented line below
-  // return getBackendSrv().get('/api/baseline');
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(baselineRecords);
-    }, 3000);
-  });
+async function loadBaselineEntries(): Promise<BaselineDTO[]> {
+  const { data: baselineRecords } = await baselineQuery._request('/api/baseline/');
+  return baselineRecords;
 }
 
 async function submitBaselineEntry(payload: BaselineEntryFields): Promise<void> {
-  // TODO: replace fabircated promise with commented line below
   await baselineQuery._post('/api/baseline/', payload);
-  const baselineRecord = {
-    ...payload,
-    id: (baselineRecords.length + 1).toString(),
-  } as BaselineDTO;
-
-  baselineRecords = [...baselineRecords, baselineRecord];
-  await new Promise<void>((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, 3000);
-  });
 }
 
 export const api = {
