f76dc3ad86aaaa8f035b0d075d9d6c00bfec048c
Kevonia <KTomlinson@realdecoy.com>
2021-09-29 12:12:40 -0500

    Update QA instance lambda endpoint in grafana app

--- public/app/core/env.ts
+++ public/app/core/env.ts
@@ -1,1 +1,1 @@
-export const API_ENDPOINT = 'https://um9jop66u4.execute-api.us-east-1.amazonaws.com/dev';
+export const API_ENDPOINT = 'https://yhil9p5ek2.execute-api.us-east-1.amazonaws.com/qa/api/baseline';
